e32c61ee0753baeea81a1b9bea9fdf6ee90de4b3
Skillzam <113600333+skillzam@users.noreply.github.com>
2025-05-20 20:20:45 +0530

    Update script1.py

--- script1.py
+++ script1.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service 
+import time
 
 # Create a new instance of the Chrome driver
 driver = webdriver.Chrome('./chromedriver')
 
 # Open the skillzam website
 driver.get("https://skillzam.com")
+
+time.sleep(30)
 
 # Close the browser window
 driver.close()
